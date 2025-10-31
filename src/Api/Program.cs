e85033e33492a417b0d4f7d6590061d1bd0fd304
lvaneenoo <lvaneenoo@proton.me>
2025-10-31 14:17:41 +0100

    Enable AOT

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,11 @@
-var builder = WebApplication.CreateBuilder();
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
 
 builder.Services.AddSingleton<ITimeSheets, TimeSheets>();
 
@@ -10,3 +17,7 @@ app.MapPost("/time-sheets/{date}/entries", TimeSheetEntriesEndpoint.PostAsync);
 app.Run();
 
 public partial class Program { }
+
+[JsonSerializable(typeof(TimeSheetEntryResource[]))]
+[JsonSerializable(typeof(TimeSheetResource[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext;
