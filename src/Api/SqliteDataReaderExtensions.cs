0955d0838d50b78e12c8f9467dff02a5f089c98d
lvaneenoo <lvaneenoo@proton.me>
2026-01-10 17:14:55 +0100

    Introduce TimeSheetResourceMaterializer

--- src/Api/SqliteDataReaderExtensions.cs
+++ src/Api/SqliteDataReaderExtensions.cs
@@ -2,6 +2,17 @@ using Microsoft.Data.Sqlite;
 
 internal static class SqliteDataReaderExtensions
 {
+    private const string TimeFormat = "HH:mm";
+
+    public static TimeSheetEntryResource? ToEntryResource(this SqliteDataReader reader) =>
+        reader.IsDBNull(2)
+            ? null
+            : new TimeSheetEntryResource
+            {
+                Id = reader.GetInt64(2),
+                Period = reader.ToPeriodResource()
+            };
+
     public static TimeSheetEntry? ToTimeSheetEntry(this SqliteDataReader reader) =>
         reader.IsDBNull(2)
             ? null
@@ -12,4 +23,16 @@ internal static class SqliteDataReaderExtensions
 
     private static Period ToPeriod(this SqliteDataReader reader) =>
         new(TimeOnly.FromDateTime(reader.GetDateTime(3)), TimeOnly.FromDateTime(reader.GetDateTime(4)));
+
+    private static PeriodResource ToPeriodResource(this SqliteDataReader reader)
+    {
+        var start = reader.GetDateTime(3);
+        var end = reader.GetDateTime(4);
+
+        return new PeriodResource
+        {
+            End = end.ToString(TimeFormat),
+            Start = start.ToString(TimeFormat)
+        };
+    }
 }
--- src/Api/TimeSheetsEndpoint.cs
+++ src/Api/TimeSheetsEndpoint.cs
@@ -1,5 +1,12 @@
 internal static class TimeSheetsEndpoint
 {
-    public static async Task<IResult> GetAsync(ITimeSheets timeSheets, TrackedDate date) =>
-        await timeSheets.FindAsync(date) is { } timeSheet ? Results.Ok(timeSheet.ToResource()) : Results.NotFound();
+    public static async Task<IResult> GetAsync(WriteStore writeStore, TrackedDate date)
+    {
+        using var command = RetrieveTimeSheets.ByTimeSheetDate(date);
+        using var reader = await writeStore.ExecuteReaderAsync(command);
+
+        var materializer = new TimeSheetResourceMaterializer(reader);
+
+        return await materializer.SingleOrDefaultAsync() is { } timeSheet ? Results.Ok(timeSheet) : Results.NotFound();
+    }
 }
